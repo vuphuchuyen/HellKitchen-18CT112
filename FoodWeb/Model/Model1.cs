80d6684cfed11009c934548c1bfe719a8e3e0ca4
nguyen <nguyen@DESKTOP-HDRRR0U>
2021-11-21 21:35:53 +0700

    update

---
+++ FoodWeb/Model/Model1.cs
@@ -0,0 +1,22 @@
+using Models.EF;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace FoodWeb.Model
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
